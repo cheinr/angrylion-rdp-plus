cheinr/angrylion-rdp-plus

commit 26b421933a6f413af66209cdc6aec88ff872dfc5

Using worker state struct pointers instead of wid index
This may speed up some parts of the code that aren't properly optimized by the compiler for some reason.

This also fixes some potential initalization issues in n64video_init.

src/core/n64video.c
@@ -131,7 +131,7 @@ static void cmd_run_buffered(uint32_t worker_id)
 {
     uint32_t pos;
     for (pos = 0; pos < rdp_cmd_buf_pos; pos++) {
-        rdp_cmd(worker_id, rdp_cmd_buf[pos]);
+        rdp_cmd(&state[worker_id], rdp_cmd_buf[pos]);
     }
 }
 
@@ -163,9 +163,15 @@ void n64video_config_init(struct n64video_config* config)
     config->dp.compat = DP_COMPAT_MEDIUM;
 }
 
-void rdp_init_worker(uint32_t worker_id)
+static void n64video_init_parallel(uint32_t worker_id)
 {
-    rdp_init(worker_id, parallel_num_workers());
+    struct rdp_state* wstate = &state[worker_id];
+
+    wstate->stride = parallel_num_workers();
+    wstate->offset = worker_id;
+    wstate->rseed = 3 + worker_id * 13;
+
+    rdp_init(wstate);
 }
 
 void n64video_init(struct n64video_config* _config)
@@ -182,11 +188,6 @@ void n64video_init(struct n64video_config* _config)
         combiner_init_lut();
         tex_init_lut();
         z_init_lut();
-
-        fb_init(0);
-        combiner_init(0);
-        tex_init(0);
-        rasterizer_init(0);
 
         static_init = true;
     }
@@ -221,9 +222,13 @@ void n64video_init(struct n64video_config* _config)
         }
 
         // init workers
-        parallel_run(rdp_init_worker);
+        parallel_run(n64video_init_parallel);
     } else {
-        rdp_init(0, 1);
+        struct rdp_state* wstate = &state[0];
+        wstate->stride = 1;
+        wstate->offset = 0;
+        wstate->rseed = 3;
+        rdp_init(wstate);
     }
 }
 
@@ -280,7 +285,7 @@ void n64video_process_list(void)
                     cmd_flush();
 
                     // parameters are unused, so NULL is fine
-                    rdp_sync_full(0, NULL);
+                    rdp_sync_full(NULL, NULL);
                 } else {
                     // increment buffer position
                     rdp_cmd_buf_pos++;
@@ -292,7 +297,7 @@ void n64video_process_list(void)
                 }
             } else {
                 // run command directly
-                rdp_cmd(0, cmd_buf);
+                rdp_cmd(&state[0], cmd_buf);
             }
 
             // send Z-buffer address to VI for "depth" output mode

src/core/n64video/rdp/blender.c
@@ -4,7 +4,7 @@ static int32_t blenderone = 0xff;
 
 static uint8_t bldiv_hwaccurate_table[0x8000];
 
-static INLINE void set_blender_input(uint32_t wid, int cycle, int which, int32_t **input_r, int32_t **input_g, int32_t **input_b, int32_t **input_a, int a, int b)
+static INLINE void set_blender_input(struct rdp_state* wstate, int cycle, int which, int32_t **input_r, int32_t **input_g, int32_t **input_b, int32_t **input_a, int a, int b)
 {
 
     switch (a & 0x3)
@@ -13,36 +13,36 @@ static INLINE void set_blender_input(uint32_t wid, int cycle, int which, int32_t
         {
             if (cycle == 0)
             {
-                *input_r = &state[wid].pixel_color.r;
-                *input_g = &state[wid].pixel_color.g;
-                *input_b = &state[wid].pixel_color.b;
+                *input_r = &wstate->pixel_color.r;
+                *input_g = &wstate->pixel_color.g;
+                *input_b = &wstate->pixel_color.b;
             }
             else
             {
-                *input_r = &state[wid].blended_pixel_color.r;
-                *input_g = &state[wid].blended_pixel_color.g;
-                *input_b = &state[wid].blended_pixel_color.b;
+                *input_r = &wstate->blended_pixel_color.r;
+                *input_g = &wstate->blended_pixel_color.g;
+                *input_b = &wstate->blended_pixel_color.b;
             }
             break;
         }
 
         case 1:
         {
-            *input_r = &state[wid].memory_color.r;
-            *input_g = &state[wid].memory_color.g;
-            *input_b = &state[wid].memory_color.b;
+            *input_r = &wstate->memory_color.r;
+            *input_g = &wstate->memory_color.g;
+            *input_b = &wstate->memory_color.b;
             break;
         }
 
         case 2:
         {
-            *input_r = &state[wid].blend_color.r;      *input_g = &state[wid].blend_color.g;      *input_b = &state[wid].blend_color.b;
+            *input_r = &wstate->blend_color.r;      *input_g = &wstate->blend_color.g;      *input_b = &wstate->blend_color.b;
             break;
         }
 
         case 3:
         {
-            *input_r = &state[wid].fog_color.r;        *input_g = &state[wid].fog_color.g;        *input_b = &state[wid].fog_color.b;
+            *input_r = &wstate->fog_color.r;        *input_g = &wstate->fog_color.g;        *input_b = &wstate->fog_color.b;
             break;
         }
     }
@@ -51,9 +51,9 @@ static INLINE void set_blender_input(uint32_t wid, int cycle, int which, int32_t
     {
         switch (b & 0x3)
         {
-            case 0:     *input_a = &state[wid].pixel_color.a; break;
-            case 1:     *input_a = &state[wid].fog_color.a; break;
-            case 2:     *input_a = &state[wid].blender_shade_alpha; break;
+            case 0:     *input_a = &wstate->pixel_color.a; break;
+            case 1:     *input_a = &wstate->fog_color.a; break;
+            case 2:     *input_a = &wstate->blender_shade_alpha; break;
             case 3:     *input_a = &zero_color; break;
         }
     }
@@ -61,25 +61,25 @@ static INLINE void set_blender_input(uint32_t wid, int cycle, int which, int32_t
     {
         switch (b & 0x3)
         {
-            case 0:     *input_a = &state[wid].inv_pixel_color.a; break;
-            case 1:     *input_a = &state[wid].memory_color.a; break;
+            case 0:     *input_a = &wstate->inv_pixel_color.a; break;
+            case 1:     *input_a = &wstate->memory_color.a; break;
             case 2:     *input_a = &blenderone; break;
             case 3:     *input_a = &zero_color; break;
         }
     }
 }
 
-static STRICTINLINE int alpha_compare(uint32_t wid, int32_t comb_alpha)
+static STRICTINLINE int alpha_compare(struct rdp_state* wstate, int32_t comb_alpha)
 {
     int32_t threshold;
-    if (!state[wid].other_modes.alpha_compare_en)
+    if (!wstate->other_modes.alpha_compare_en)
         return 1;
     else
     {
-        if (!state[wid].other_modes.dither_alpha_en)
-            threshold = state[wid].blend_color.a;
+        if (!wstate->other_modes.dither_alpha_en)
+            threshold = wstate->blend_color.a;
         else
-            threshold = irand(&state[wid].rseed) & 0xff;
+            threshold = irand(&wstate->rseed) & 0xff;
 
 
         if (comb_alpha >= threshold)
@@ -89,33 +89,33 @@ static STRICTINLINE int alpha_compare(uint32_t wid, int32_t comb_alpha)
     }
 }
 
-static STRICTINLINE void blender_equation_cycle0(uint32_t wid, int* r, int* g, int* b)
+static STRICTINLINE void blender_equation_cycle0(struct rdp_state* wstate, int* r, int* g, int* b)
 {
     int blend1a, blend2a;
     int blr, blg, blb, sum;
-    blend1a = *state[wid].blender1b_a[0] >> 3;
-    blend2a = *state[wid].blender2b_a[0] >> 3;
+    blend1a = *wstate->blender1b_a[0] >> 3;
+    blend2a = *wstate->blender2b_a[0] >> 3;
 
     int mulb;
 
 
 
-    if (state[wid].blender2b_a[0] == &state[wid].memory_color.a)
+    if (wstate->blender2b_a[0] == &wstate->memory_color.a)
     {
-        blend1a = (blend1a >> state[wid].blshifta) & 0x3C;
-        blend2a = (blend2a >> state[wid].blshiftb) | 3;
+        blend1a = (blend1a >> wstate->blshifta) & 0x3C;
+        blend2a = (blend2a >> wstate->blshiftb) | 3;
     }
 
     mulb = blend2a + 1;
 
 
-    blr = (*state[wid].blender1a_r[0]) * blend1a + (*state[wid].blender2a_r[0]) * mulb;
-    blg = (*state[wid].blender1a_g[0]) * blend1a + (*state[wid].blender2a_g[0]) * mulb;
-    blb = (*state[wid].blender1a_b[0]) * blend1a + (*state[wid].blender2a_b[0]) * mulb;
+    blr = (*wstate->blender1a_r[0]) * blend1a + (*wstate->blender2a_r[0]) * mulb;
+    blg = (*wstate->blender1a_g[0]) * blend1a + (*wstate->blender2a_g[0]) * mulb;
+    blb = (*wstate->blender1a_b[0]) * blend1a + (*wstate->blender2a_b[0]) * mulb;
 
 
 
-    if (!state[wid].other_modes.force_blend)
+    if (!wstate->other_modes.force_blend)
     {
 
 
@@ -135,44 +135,44 @@ static STRICTINLINE void blender_equation_cycle0(uint32_t wid, int* r, int* g, i
     }
 }
 
-static STRICTINLINE void blender_equation_cycle0_2(uint32_t wid, int* r, int* g, int* b)
+static STRICTINLINE void blender_equation_cycle0_2(struct rdp_state* wstate, int* r, int* g, int* b)
 {
     int blend1a, blend2a;
-    blend1a = *state[wid].blender1b_a[0] >> 3;
-    blend2a = *state[wid].blender2b_a[0] >> 3;
+    blend1a = *wstate->blender1b_a[0] >> 3;
+    blend2a = *wstate->blender2b_a[0] >> 3;
 
-    if (state[wid].blender2b_a[0] == &state[wid].memory_color.a)
+    if (wstate->blender2b_a[0] == &wstate->memory_color.a)
     {
-        blend1a = (blend1a >> state[wid].pastblshifta) & 0x3C;
-        blend2a = (blend2a >> state[wid].pastblshiftb) | 3;
+        blend1a = (blend1a >> wstate->pastblshifta) & 0x3C;
+        blend2a = (blend2a >> wstate->pastblshiftb) | 3;
     }
 
     blend2a += 1;
-    *r = (((*state[wid].blender1a_r[0]) * blend1a + (*state[wid].blender2a_r[0]) * blend2a) >> 5) & 0xff;
-    *g = (((*state[wid].blender1a_g[0]) * blend1a + (*state[wid].blender2a_g[0]) * blend2a) >> 5) & 0xff;
-    *b = (((*state[wid].blender1a_b[0]) * blend1a + (*state[wid].blender2a_b[0]) * blend2a) >> 5) & 0xff;
+    *r = (((*wstate->blender1a_r[0]) * blend1a + (*wstate->blender2a_r[0]) * blend2a) >> 5) & 0xff;
+    *g = (((*wstate->blender1a_g[0]) * blend1a + (*wstate->blender2a_g[0]) * blend2a) >> 5) & 0xff;
+    *b = (((*wstate->blender1a_b[0]) * blend1a + (*wstate->blender2a_b[0]) * blend2a) >> 5) & 0xff;
 }
 
-static STRICTINLINE void blender_equation_cycle1(uint32_t wid, int* r, int* g, int* b)
+static STRICTINLINE void blender_equation_cycle1(struct rdp_state* wstate, int* r, int* g, int* b)
 {
     int blend1a, blend2a;
     int blr, blg, blb, sum;
-    blend1a = *state[wid].blender1b_a[1] >> 3;
-    blend2a = *state[wid].blender2b_a[1] >> 3;
+    blend1a = *wstate->blender1b_a[1] >> 3;
+    blend2a = *wstate->blender2b_a[1] >> 3;
 
     int mulb;
-    if (state[wid].blender2b_a[1] == &state[wid].memory_color.a)
+    if (wstate->blender2b_a[1] == &wstate->memory_color.a)
     {
-        blend1a = (blend1a >> state[wid].blshifta) & 0x3C;
-        blend2a = (blend2a >> state[wid].blshiftb) | 3;
+        blend1a = (blend1a >> wstate->blshifta) & 0x3C;
+        blend2a = (blend2a >> wstate->blshiftb) | 3;
     }
 
     mulb = blend2a + 1;
-    blr = (*state[wid].blender1a_r[1]) * blend1a + (*state[wid].blender2a_r[1]) * mulb;
-    blg = (*state[wid].blender1a_g[1]) * blend1a + (*state[wid].blender2a_g[1]) * mulb;
-    blb = (*state[wid].blender1a_b[1]) * blend1a + (*state[wid].blender2a_b[1]) * mulb;
+    blr = (*wstate->blender1a_r[1]) * blend1a + (*wstate->blender2a_r[1]) * mulb;
+    blg = (*wstate->blender1a_g[1]) * blend1a + (*wstate->blender2a_g[1]) * mulb;
+    blb = (*wstate->blender1a_b[1]) * blend1a + (*wstate->blender2a_b[1]) * mulb;
 
-    if (!state[wid].other_modes.force_blend)
+    if (!wstate->other_modes.force_blend)
     {
         sum = ((blend1a & ~3) + (blend2a & ~3) + 4) << 9;
         *r = bldiv_hwaccurate_table[sum | ((blr >> 2) & 0x7ff)];
@@ -187,12 +187,12 @@ static STRICTINLINE void blender_equation_cycle1(uint32_t wid, int* r, int* g, i
     }
 }
 
-static STRICTINLINE int blender_1cycle(uint32_t wid, uint32_t* fr, uint32_t* fg, uint32_t* fb, int dith, uint32_t blend_en, uint32_t prewrap, uint32_t curpixel_cvg, uint32_t curpixel_cvbit)
+static STRICTINLINE int blender_1cycle(struct rdp_state* wstate, uint32_t* fr, uint32_t* fg, uint32_t* fb, int dith, uint32_t blend_en, uint32_t prewrap, uint32_t curpixel_cvg, uint32_t curpixel_cvbit)
 {
     int r, g, b, dontblend;
 
 
-    if (alpha_compare(wid, state[wid].pixel_color.a))
+    if (alpha_compare(wstate, wstate->pixel_color.a))
     {
 
 
@@ -200,38 +200,38 @@ static STRICTINLINE int blender_1cycle(uint32_t wid, uint32_t* fr, uint32_t* fg,
 
 
 
-        if (state[wid].other_modes.antialias_en ? curpixel_cvg : curpixel_cvbit)
+        if (wstate->other_modes.antialias_en ? curpixel_cvg : curpixel_cvbit)
         {
 
-            if (!state[wid].other_modes.color_on_cvg || prewrap)
+            if (!wstate->other_modes.color_on_cvg || prewrap)
             {
-                dontblend = (state[wid].other_modes.f.partialreject_1cycle && state[wid].pixel_color.a >= 0xff);
+                dontblend = (wstate->other_modes.f.partialreject_1cycle && wstate->pixel_color.a >= 0xff);
                 if (!blend_en || dontblend)
                 {
-                    r = *state[wid].blender1a_r[0];
-                    g = *state[wid].blender1a_g[0];
-                    b = *state[wid].blender1a_b[0];
+                    r = *wstate->blender1a_r[0];
+                    g = *wstate->blender1a_g[0];
+                    b = *wstate->blender1a_b[0];
                 }
                 else
                 {
-                    state[wid].inv_pixel_color.a =  (~(*state[wid].blender1b_a[0])) & 0xff;
+                    wstate->inv_pixel_color.a =  (~(*wstate->blender1b_a[0])) & 0xff;
 
 
 
 
 
-                    blender_equation_cycle0(wid, &r, &g, &b);
+                    blender_equation_cycle0(wstate, &r, &g, &b);
                 }
             }
             else
             {
-                r = *state[wid].blender2a_r[0];
-                g = *state[wid].blender2a_g[0];
-                b = *state[wid].blender2a_b[0];
+                r = *wstate->blender2a_r[0];
+                g = *wstate->blender2a_g[0];
+                b = *wstate->blender2a_b[0];
             }
 
-            if (state[wid].other_modes.rgb_dither_sel != 3)
-                rgb_dither(state[wid].other_modes.rgb_dither_sel, &r, &g, &b, dith);
+            if (wstate->other_modes.rgb_dither_sel != 3)
+                rgb_dither(wstate->other_modes.rgb_dither_sel, &r, &g, &b, dith);
 
             *fr = r;
             *fg = g;
@@ -245,56 +245,56 @@ static STRICTINLINE int blender_1cycle(uint32_t wid, uint32_t* fr, uint32_t* fg,
         return 0;
 }
 
-static STRICTINLINE int blender_2cycle_cycle0(uint32_t wid, uint32_t curpixel_cvg, uint32_t curpixel_cvbit)
+static STRICTINLINE int blender_2cycle_cycle0(struct rdp_state* wstate, uint32_t curpixel_cvg, uint32_t curpixel_cvbit)
 {
     int r, g, b;
-    int wen = (state[wid].other_modes.antialias_en ? curpixel_cvg : curpixel_cvbit) > 0 ? 1 : 0;
+    int wen = (wstate->other_modes.antialias_en ? curpixel_cvg : curpixel_cvbit) > 0 ? 1 : 0;
 
     if (wen)
     {
-        state[wid].inv_pixel_color.a =  (~(*state[wid].blender1b_a[0])) & 0xff;
+        wstate->inv_pixel_color.a =  (~(*wstate->blender1b_a[0])) & 0xff;
 
-        blender_equation_cycle0_2(wid, &r, &g, &b);
+        blender_equation_cycle0_2(wstate, &r, &g, &b);
 
-        state[wid].blended_pixel_color.r = r;
-        state[wid].blended_pixel_color.g = g;
-        state[wid].blended_pixel_color.b = b;
+        wstate->blended_pixel_color.r = r;
+        wstate->blended_pixel_color.g = g;
+        wstate->blended_pixel_color.b = b;
     }
 
-    state[wid].memory_color = state[wid].pre_memory_color;
+    wstate->memory_color = wstate->pre_memory_color;
 
     return wen;
 }
 
 
-static STRICTINLINE void blender_2cycle_cycle1(uint32_t wid, uint32_t* fr, uint32_t* fg, uint32_t* fb, int dith, uint32_t blend_en, uint32_t prewrap)
+static STRICTINLINE void blender_2cycle_cycle1(struct rdp_state* wstate, uint32_t* fr, uint32_t* fg, uint32_t* fb, int dith, uint32_t blend_en, uint32_t prewrap)
 {
     int r, g, b, dontblend;
 
-    if (!state[wid].other_modes.color_on_cvg || prewrap)
+    if (!wstate->other_modes.color_on_cvg || prewrap)
     {
-        dontblend = (state[wid].other_modes.f.partialreject_2cycle && state[wid].pixel_color.a >= 0xff);
+        dontblend = (wstate->other_modes.f.partialreject_2cycle && wstate->pixel_color.a >= 0xff);
         if (!blend_en || dontblend)
         {
-            r = *state[wid].blender1a_r[1];
-            g = *state[wid].blender1a_g[1];
-            b = *state[wid].blender1a_b[1];
+            r = *wstate->blender1a_r[1];
+            g = *wstate->blender1a_g[1];
+            b = *wstate->blender1a_b[1];
         }
         else
         {
-            state[wid].inv_pixel_color.a =  (~(*state[wid].blender1b_a[1])) & 0xff;
-            blender_equation_cycle1(wid, &r, &g, &b);
+            wstate->inv_pixel_color.a =  (~(*wstate->blender1b_a[1])) & 0xff;
+            blender_equation_cycle1(wstate, &r, &g, &b);
         }
     }
     else
     {
-        r = *state[wid].blender2a_r[1];
-        g = *state[wid].blender2a_g[1];
-        b = *state[wid].blender2a_b[1];
+        r = *wstate->blender2a_r[1];
+        g = *wstate->blender2a_g[1];
+        b = *wstate->blender2a_b[1];
     }
 
-    if (state[wid].other_modes.rgb_dither_sel != 3)
-        rgb_dither(state[wid].other_modes.rgb_dither_sel, &r, &g, &b, dith);
+    if (wstate->other_modes.rgb_dither_sel != 3)
+        rgb_dither(wstate->other_modes.rgb_dither_sel, &r, &g, &b, dith);
 
     *fr = r;
     *fg = g;
@@ -331,20 +331,20 @@ static void blender_init_lut(void)
     }
 }
 
-void rdp_set_fog_color(uint32_t wid, const uint32_t* args)
+void rdp_set_fog_color(struct rdp_state* wstate, const uint32_t* args)
 {
-    state[wid].fog_color.r = RGBA32_R(args[1]);
-    state[wid].fog_color.g = RGBA32_G(args[1]);
-    state[wid].fog_color.b = RGBA32_B(args[1]);
-    state[wid].fog_color.a = RGBA32_A(args[1]);
+    wstate->fog_color.r = RGBA32_R(args[1]);
+    wstate->fog_color.g = RGBA32_G(args[1]);
+    wstate->fog_color.b = RGBA32_B(args[1]);
+    wstate->fog_color.a = RGBA32_A(args[1]);
 }
 
-void rdp_set_blend_color(uint32_t wid, const uint32_t* args)
+void rdp_set_blend_color(struct rdp_state* wstate, const uint32_t* args)
 {
-    state[wid].blend_color.r = RGBA32_R(args[1]);
-    state[wid].blend_color.g = RGBA32_G(args[1]);
-    state[wid].blend_color.b = RGBA32_B(args[1]);
-    state[wid].blend_color.a = RGBA32_A(args[1]);
+    wstate->blend_color.r = RGBA32_R(args[1]);
+    wstate->blend_color.g = RGBA32_G(args[1]);
+    wstate->blend_color.b = RGBA32_B(args[1]);
+    wstate->blend_color.a = RGBA32_A(args[1]);
 }
 
 #endif // N64VIDEO_C

src/core/n64video/rdp/coverage.c
@@ -29,14 +29,14 @@ static STRICTINLINE uint32_t leftcvghex(uint32_t x, uint32_t fmask)
 
 
 
-static STRICTINLINE void compute_cvg_flip(uint32_t wid, int32_t scanline)
+static STRICTINLINE void compute_cvg_flip(struct rdp_state* wstate, int32_t scanline)
 {
     int32_t purgestart, purgeend;
     int i, length, fmask, maskshift, fmaskshifted;
     int32_t minorcur, majorcur, minorcurint, majorcurint, samecvg;
 
-    purgestart = state[wid].span[scanline].rx;
-    purgeend = state[wid].span[scanline].lx;
+    purgestart = wstate->span[scanline].rx;
+    purgeend = wstate->span[scanline].lx;
     length = purgeend - purgestart;
     if (length >= 0)
     {
@@ -46,7 +46,7 @@ static STRICTINLINE void compute_cvg_flip(uint32_t wid, int32_t scanline)
 
 
 
-        memset(&state[wid].cvgbuf[purgestart], 0xff, length + 1);
+        memset(&wstate->cvgbuf[purgestart], 0xff, length + 1);
         for(i = 0; i < 4; i++)
         {
 
@@ -58,19 +58,19 @@ static STRICTINLINE void compute_cvg_flip(uint32_t wid, int32_t scanline)
                 maskshift = (i - 2) & 4;
                 fmaskshifted = fmask << maskshift;
 
-                if (!state[wid].span[scanline].invalyscan[i])
+                if (!wstate->span[scanline].invalyscan[i])
                 {
                    int k;
-                    minorcur = state[wid].span[scanline].minorx[i];
-                    majorcur = state[wid].span[scanline].majorx[i];
+                    minorcur = wstate->span[scanline].minorx[i];
+                    majorcur = wstate->span[scanline].majorx[i];
                     minorcurint = minorcur >> 3;
                     majorcurint = majorcur >> 3;
 
 
                     for (k = purgestart; k <= majorcurint; k++)
-                        state[wid].cvgbuf[k] &= ~fmaskshifted;
+                        wstate->cvgbuf[k] &= ~fmaskshifted;
                     for (k = minorcurint; k <= purgeend; k++)
-                        state[wid].cvgbuf[k] &= ~fmaskshifted;
+                        wstate->cvgbuf[k] &= ~fmaskshifted;
 
 
 
@@ -82,20 +82,20 @@ static STRICTINLINE void compute_cvg_flip(uint32_t wid, int32_t scanline)
 
                     if (minorcurint > majorcurint)
                     {
-                        state[wid].cvgbuf[minorcurint] |= (rightcvghex(minorcur, fmask) << maskshift);
-                        state[wid].cvgbuf[majorcurint] |= (leftcvghex(majorcur, fmask) << maskshift);
+                        wstate->cvgbuf[minorcurint] |= (rightcvghex(minorcur, fmask) << maskshift);
+                        wstate->cvgbuf[majorcurint] |= (leftcvghex(majorcur, fmask) << maskshift);
                     }
                     else if (minorcurint == majorcurint)
                     {
                         samecvg = rightcvghex(minorcur, fmask) & leftcvghex(majorcur, fmask);
-                        state[wid].cvgbuf[majorcurint] |= (samecvg << maskshift);
+                        wstate->cvgbuf[majorcurint] |= (samecvg << maskshift);
                     }
                 }
                 else
                 {
                     int k;
                     for (k = purgestart; k <= purgeend; k++)
-                        state[wid].cvgbuf[k] &= ~fmaskshifted;
+                        wstate->cvgbuf[k] &= ~fmaskshifted;
                 }
 
         }
@@ -104,19 +104,19 @@ static STRICTINLINE void compute_cvg_flip(uint32_t wid, int32_t scanline)
 
 }
 
-static STRICTINLINE void compute_cvg_noflip(uint32_t wid, int32_t scanline)
+static STRICTINLINE void compute_cvg_noflip(struct rdp_state* wstate, int32_t scanline)
 {
     int32_t purgestart, purgeend;
     int i, length, fmask, maskshift, fmaskshifted;
     int32_t minorcur, majorcur, minorcurint, majorcurint, samecvg;
 
-    purgestart = state[wid].span[scanline].lx;
-    purgeend = state[wid].span[scanline].rx;
+    purgestart = wstate->span[scanline].lx;
+    purgeend = wstate->span[scanline].rx;
     length = purgeend - purgestart;
 
     if (length >= 0)
     {
-        memset(&state[wid].cvgbuf[purgestart], 0xff, length + 1);
+        memset(&wstate->cvgbuf[purgestart], 0xff, length + 1);
 
         for(i = 0; i < 4; i++)
         {
@@ -124,35 +124,35 @@ static STRICTINLINE void compute_cvg_noflip(uint32_t wid, int32_t scanline)
             maskshift = (i - 2) & 4;
             fmaskshifted = fmask << maskshift;
 
-            if (!state[wid].span[scanline].invalyscan[i])
+            if (!wstate->span[scanline].invalyscan[i])
             {
                int k;
-                minorcur = state[wid].span[scanline].minorx[i];
-                majorcur = state[wid].span[scanline].majorx[i];
+                minorcur = wstate->span[scanline].minorx[i];
+                majorcur = wstate->span[scanline].majorx[i];
                 minorcurint = minorcur >> 3;
                 majorcurint = majorcur >> 3;
 
                 for (k = purgestart; k <= minorcurint; k++)
-                    state[wid].cvgbuf[k] &= ~fmaskshifted;
+                    wstate->cvgbuf[k] &= ~fmaskshifted;
                 for (k = majorcurint; k <= purgeend; k++)
-                    state[wid].cvgbuf[k] &= ~fmaskshifted;
+                    wstate->cvgbuf[k] &= ~fmaskshifted;
 
                 if (majorcurint > minorcurint)
                 {
-                    state[wid].cvgbuf[minorcurint] |= (leftcvghex(minorcur, fmask) << maskshift);
-                    state[wid].cvgbuf[majorcurint] |= (rightcvghex(majorcur, fmask) << maskshift);
+                    wstate->cvgbuf[minorcurint] |= (leftcvghex(minorcur, fmask) << maskshift);
+                    wstate->cvgbuf[majorcurint] |= (rightcvghex(majorcur, fmask) << maskshift);
                 }
                 else if (minorcurint == majorcurint)
                 {
                     samecvg = leftcvghex(minorcur, fmask) & rightcvghex(majorcur, fmask);
-                    state[wid].cvgbuf[majorcurint] |= (samecvg << maskshift);
+                    wstate->cvgbuf[majorcurint] |= (samecvg << maskshift);
                 }
             }
             else
             {
                 int k;
                 for (k = purgestart; k <= purgeend; k++)
-                    state[wid].cvgbuf[k] &= ~fmaskshifted;
+                    wstate->cvgbuf[k] &= ~fmaskshifted;
             }
         }
     }

src/core/n64video/rdp/dither.c
@@ -64,15 +64,15 @@ static STRICTINLINE void rgb_dither(int rgb_dither_sel, int* r, int* g, int* b, 
     *b = *b + (ditherdiff & replacesign);
 }
 
-static STRICTINLINE void get_dither_noise(uint32_t wid, int x, int y, int* cdith, int* adith)
+static STRICTINLINE void get_dither_noise(struct rdp_state* wstate, int x, int y, int* cdith, int* adith)
 {
-    if (!state[wid].other_modes.f.getditherlevel)
-        state[wid].noise = ((irand(&state[wid].rseed) & 7) << 6) | 0x20;
+    if (!wstate->other_modes.f.getditherlevel)
+        wstate->noise = ((irand(&wstate->rseed) & 7) << 6) | 0x20;
 
-    y >>= state[wid].scfield;
+    y >>= wstate->scfield;
 
     int dithindex;
-    switch(state[wid].other_modes.f.rgb_alpha_dither)
+    switch(wstate->other_modes.f.rgb_alpha_dither)
     {
     case 0:
         dithindex = ((y & 3) << 2) | (x & 3);
@@ -86,7 +86,7 @@ static STRICTINLINE void get_dither_noise(uint32_t wid, int x, int y, int* cdith
     case 2:
         dithindex = ((y & 3) << 2) | (x & 3);
         *cdith = magic_matrix[dithindex];
-        *adith = (state[wid].noise >> 6) & 7;
+        *adith = (wstate->noise >> 6) & 7;
         break;
     case 3:
         dithindex = ((y & 3) << 2) | (x & 3);
@@ -105,7 +105,7 @@ static STRICTINLINE void get_dither_noise(uint32_t wid, int x, int y, int* cdith
     case 6:
         dithindex = ((y & 3) << 2) | (x & 3);
         *cdith = bayer_matrix[dithindex];
-        *adith = (state[wid].noise >> 6) & 7;
+        *adith = (wstate->noise >> 6) & 7;
         break;
     case 7:
         dithindex = ((y & 3) << 2) | (x & 3);
@@ -114,20 +114,20 @@ static STRICTINLINE void get_dither_noise(uint32_t wid, int x, int y, int* cdith
         break;
     case 8:
         dithindex = ((y & 3) << 2) | (x & 3);
-        *cdith = irand(&state[wid].rseed);
+        *cdith = irand(&wstate->rseed);
         *adith = magic_matrix[dithindex];
         break;
     case 9:
         dithindex = ((y & 3) << 2) | (x & 3);
-        *cdith = irand(&state[wid].rseed);
+        *cdith = irand(&wstate->rseed);
         *adith = (~magic_matrix[dithindex]) & 7;
         break;
     case 10:
-        *cdith = irand(&state[wid].rseed);
-        *adith = (state[wid].noise >> 6) & 7;
+        *cdith = irand(&wstate->rseed);
+        *adith = (wstate->noise >> 6) & 7;
         break;
     case 11:
-        *cdith = irand(&state[wid].rseed);
+        *cdith = irand(&wstate->rseed);
         *adith = 0;
         break;
     case 12:
@@ -142,7 +142,7 @@ static STRICTINLINE void get_dither_noise(uint32_t wid, int x, int y, int* cdith
         break;
     case 14:
         *cdith = 7;
-        *adith = (state[wid].noise >> 6) & 7;
+        *adith = (wstate->noise >> 6) & 7;
         break;
     case 15:
         *cdith = 7;

commit 0d6235acbf06015125aebb4b4a2d4d4957deb4f6

Fixed regression caused by 26b42193
Re-initalizing the RDP state every time can cause issues
with save states, so the rdp_init call has been moved to
the static init block.

src/core/n64video.c
@@ -169,8 +169,6 @@ static void n64video_init_parallel(uint32_t worker_id)
     wstate->stride = parallel_num_workers();
     wstate->offset = worker_id;
     wstate->rseed = 3 + worker_id * 13;
-
-    rdp_init(wstate);
 }
 
 void n64video_init(struct n64video_config* _config)
@@ -187,6 +185,10 @@ void n64video_init(struct n64video_config* _config)
         combiner_init_lut();
         tex_init_lut();
         z_init_lut();
+
+        for (uint32_t i = 1; i < PARALLEL_MAX_WORKERS; i++) {
+            rdp_init(&state[i]);
+        }
 
         static_init = true;
     }
@@ -231,7 +233,6 @@ void n64video_init(struct n64video_config* _config)
         wstate->stride = 1;
         wstate->offset = 0;
         wstate->rseed = 3;
-        rdp_init(wstate);
     }
 }
 